feea17842a3a90c31cb4429bc275dbb89911cceb
Ramil Galikeev <galikeevramil95@gmail.com>
2022-05-19 08:40:18 +0500

    implementation of the transfer of a move from one player to another

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import BoardComponent from "./components/boardComponent/BoardComponent";
 import { Board } from "./models/Board";
+import { Colors } from "./models/Colors";
+import { Player } from "./models/Player";
 
 
 const App = () => {
 
 	const [board, setBoard] = useState(new Board());
+	const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+	const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+	const [currentPlayer, setCurrentPlayer] = useState <Player | null>(null);
 
 	useEffect(() => {
 		restart();
+		setCurrentPlayer(whitePlayer);
 	}, [])
 
 	const restart = () => {
@@ -18,9 +24,18 @@ const App = () => {
 		setBoard(newBoard);
 	}
 
+	const swapPlayer = () => {
+		setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
+	}
+
 	return (
 		<div className="app">
-			<BoardComponent board={board} setBoard={setBoard}/>
+			<BoardComponent 
+				board={board} 
+				setBoard={setBoard}
+				currentPlayer={currentPlayer}
+				swapPlayer={swapPlayer}
+			/>
 		</div>
 	);
 }
